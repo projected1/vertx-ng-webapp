f7980511c79be20697ffd83bbc8c588de280cda3
Daniel Gorbatov <daniel.gorbatov@gmail.com>
2018-07-21 22:10:42 +0300

    Downgrade Angular from 6.x to 5.x
    vertx3-eventbus-client module, does not support Angular 6.x.

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ target_dir='/home/ubuntu/vala-demo-ng'
 ng build --prod
 chmod 400 $key
 ssh -i $key -f $svr "mkdir -p $target_dir"
-scp -i $key -rp dist/vala-demo-ng/* $svr:$target_dir
+scp -i $key -rp dist/* $svr:$target_dir
